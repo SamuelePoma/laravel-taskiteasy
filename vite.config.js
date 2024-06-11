8ac663c8ac82bb10044015f3f9abc35c04ae8ab0
SamuelePoma <poma0001@hz.nl>
2024-06-11 16:58:50 +0200

    Fix mixed content issue by loading assets over HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -8,8 +9,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
+    server: {
+        https: true,
     },
 });
