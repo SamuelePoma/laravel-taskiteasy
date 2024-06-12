68d2ae1ea59e076b5d30f2daffe79860ba6cd465
SamuelePoma <poma0001@hz.nl>
2024-06-12 18:24:12 +0200

    404 page done

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,6 @@ Route::resource('projects', ProjectController::class);
  * Route that shows the about page. This handler just returns the about view.
  */
 Route::view('/about', 'about')->name('about');
-Route::fallback(function () {
+Route::get('/404', function () {
     return view('errors.404');
 })->name('error.404');
